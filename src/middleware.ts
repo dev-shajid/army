ec712a4e28be7b99f4723659f44300799a444857
dev-shajid <sajidislam729@gmail.com>
2025-10-20 18:03:51 +0600

    feat: replace createServerClient with createClient in middleware for improved authentication handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 import { AUTH_REDIRECT_URL, DEFAULT_REDIRECT_URL, isAuthRoute, isPublicRoute } from './route'
+import { createClient } from './supabase/server'
 
 
 export async function middleware(request: NextRequest) {
@@ -9,26 +9,7 @@ export async function middleware(request: NextRequest) {
         request,
     })
 
-    const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-        {
-            cookies: {
-                getAll() {
-                    return request.cookies.getAll()
-                },
-                setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
-                    supabaseResponse = NextResponse.next({
-                        request,
-                    })
-                    cookiesToSet.forEach(({ name, value, options }) =>
-                        supabaseResponse.cookies.set(name, value, options)
-                    )
-                },
-            },
-        }
-    )
+    const supabase = await createClient()
 
     // Do not run code between createServerClient and
     // supabase.auth.getUser(). A simple mistake could make it very hard to debug
