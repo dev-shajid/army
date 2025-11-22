07b41d1d3078edd9f67562a21e944f00bb15ef3b
dev-shajid <sajidislam729@gmail.com>
2025-11-22 18:36:26 +0600

    feat: add FolderCard component to PolicyPage for future use

--- src/app/(dashboard)/sop-policy/policy/page.tsx
+++ src/app/(dashboard)/sop-policy/policy/page.tsx
@@ -1,4 +1,6 @@
+import { FolderCard } from '@/components/folder-card'
 import PageHeader from '@/components/page-header'
+import PDFCard from '@/components/pdf-card'
 
 export default function PolicyPage() {
   return (
@@ -7,6 +9,16 @@ export default function PolicyPage() {
         title="SOP Policy"
         description="Access Standard Operating Procedures organized by branch and role"
       />
+      <div className='mt-12'>
+        {/* <FolderCard
+          href={"/sop-policy/sop/ipft"}
+          title={"IPFT"}
+          description=""
+        /> */}
+        <PDFCard
+          doc={{ title: "IPFT", fileId: "1pmNPgsw3BM8FiSHUXX2Qa33q5yg_KeAj" }}
+        />
+      </div>
     </>
   )
 }
